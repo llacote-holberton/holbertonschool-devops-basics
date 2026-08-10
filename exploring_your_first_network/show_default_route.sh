8386afbfab1b3ef9ec13eb601bad2e7ca9da7682
Laurent Lacôte <laurent.lacote@private.com>
2026-08-10 16:16:16 +0200

    EFN - T3 - Showing the routing's fallback strategy

---
+++ exploring_your_first_network/show_default_route.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+# -Br for "brief" so reduced output
+# Route to list the routing strategy
+# Show default to only show the fallback target
+# Cut to further reduce output to what we only really want
+ip -br route show default | cut -d ' ' -f -5
