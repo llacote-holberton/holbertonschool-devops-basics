d65a3e3ca8829a4bc37d5094da69c983ed9756d7
Laurent Lacôte <laurent.lacote@private.com>
2026-08-10 16:20:11 +0200

    EFN - T3 - Trying another writing
    In hope it will work for checker even though
      it shows more information technically.

--- exploring_your_first_network/show_default_route.sh
+++ exploring_your_first_network/show_default_route.sh
@@ -3,4 +3,5 @@
 # Route to list the routing strategy
 # Show default to only show the fallback target
 # Cut to further reduce output to what we only really want
-ip -br route show default | cut -d ' ' -f -5
+# ip -br route show default | cut -d ' ' -f -5
+ip -4 route show default
